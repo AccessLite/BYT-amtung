4c825476a9e27a8bd13b0de5bb544c47cc43ee35
Annie <annie.mmt@gmail.com>
2016-11-23 00:21:18 -0500

    Trouble with FoaasOperation model

--- FOAAS/FOAAS/FoaasOperation.swift
+++ FOAAS/FOAAS/FoaasOperation.swift
@@ -12,24 +12,29 @@ class FoaasOperation: JSONConvertible, DataConvertible {
     
     let name: String
     let url: String
+    let fields: [[String : AnyObject]]
     
-    init(name: String, url: String) {
+    init(name: String, url: String, fields: [[String : AnyObject]]) {
         self.name = name
         self.url = url
-        }
+        self.fields = fields
+    }
     
     convenience required init?(json: [String: AnyObject]) {
         guard let name = json["name"] as? String else { return nil }
         guard let url = json["url"] as? String else { return nil }
-        self.init(name: name, url: url)
+        guard let fields = json["fields"] as? [[String : AnyObject]] else { return nil }
+        self.init(name: name, url: url, fields: fields)
     }
     
-    func toJson() -> [String : AnyObject] {
-        let json: [String : AnyObject] = [
-            "name" : name as AnyObject,
-            "url" : url as AnyObject
-        ]
-        return json
+    convenience required init?(data: Data) {
+        do {
+            let jsonData = try JSONSerialization.jsonObject(with: data, options: [])
+            guard jsonData is [String : AnyObject] else { return nil }
+            self.init(jsonData)
+        } catch {
+            print("Error converting to data")
+        }
     }
     
     func toData() throws -> Data {
@@ -39,6 +44,12 @@ class FoaasOperation: JSONConvertible, DataConvertible {
             throw NSError(domain: "Error converting to data", code: 1, userInfo: nil)
         }
     }
-
     
+    func toJson() -> [String : AnyObject] {
+        let json: [String : AnyObject] = [
+            "name" : name as AnyObject,
+            "url" : url as AnyObject
+        ]
+        return json
+    }
 }
