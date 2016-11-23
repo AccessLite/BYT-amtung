d7f39b26b84ddfa8f6ac39cc8bb947937fadff7e
Annie <annie.mmt@gmail.com>
2016-11-22 23:18:21 -0500

    Finished Foaas model

--- FOAAS/FOAAS/Foaas.swift
+++ FOAAS/FOAAS/Foaas.swift
@@ -13,19 +13,28 @@ class Foaas: JSONConvertible, CustomStringConvertible {
     let message: String
     let subtitle: String
     
-    required init?(json: [String : AnyObject]) {
-        if
-            let message = json["message"] as? String,
-            let subtitle = json["subtitle"] as? String {
-            self.message = message
-            self.subtitle = subtitle
-        } else {
-            return nil
-        }
+    var description: String {
+        return "\(message) + \(subtitle)"
+    }
+    
+    init(message: String, subtitle: String) {
+        self.message = message
+        self.subtitle = subtitle
+    }
+    
+    convenience required init?(json: [String : AnyObject]) {
+        guard let message = json["message"] as? String else { return nil }
+        guard let subtitle = json["subtitle"] as? String else { return nil }
+        self.init(message: message, subtitle: subtitle)
     }
     
     func toJson() -> [String : AnyObject] {
-        <#code#>
+        let myDict: [String : AnyObject] = [
+            "message" : message as AnyObject,
+            "subtitle" : subtitle as AnyObject
+        ]
+        return myDict
     }
 }
+
 
